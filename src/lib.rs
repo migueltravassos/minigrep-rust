9e927b2030bdfb26a5bfa026fa00fbef9216333d
Miguel Travassos <miguel.ft1706@gmail.com>
2026-03-20 10:48:22 +0000

    Added lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+        use std::env;
+        use std::error::Error;
+        use std::fs;
+ 
+        pub struct Config{
+            word: String,
+            file: String,
+            ignore_case: bool,
+        }
+
+        impl Config{
+            pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str>{
+                args.next(); // path
+
+                let word = match args.next(){
+                    Some(arg) => arg,
+                    None => return Err("Please enter a word!"),
+                };
+
+                let file = match args.next(){
+                    Some(arg) => arg,
+                    None => return Err("Please enter a file!"),
+                };
+
+                let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+                Ok(Config { 
+                    word,
+                    file,   
+                    ignore_case,
+                })
+            }
+        }
+
+
+        fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
+            let query = query.to_lowercase();
+
+            content
+                .lines()
+                .filter(| line | line.to_lowercase().contains(&query))
+                .collect()
+        }
+
+        
+        
+        fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str>{   
+            content
+                .lines()
+                .filter(|line| line.contains(query))
+                .collect()
+        }
+
+        
+        pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+            let content: String = fs::read_to_string(&config.file)?;
+
+            let results = if config.ignore_case{
+                search_case_insensitive(&config.word, &content)
+            }else{
+                search(&config.word, &content)
+            };
+
+            for line in results{
+                    println!("{}", line);
+            } 
+            Ok(())
+        }
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,6 @@
-        use std::env;
-        use std::error::Error;
-        use std::fs;
-        use std::process;
- 
-        struct Config{
-            word: String,
-            file: String,
-            ignore_case: bool,
-        }
-
-        impl Config{
-            fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str>{
-                args.next(); // path
-
-                let word = match args.next(){
-                    Some(arg) => arg,
-                    None => return Err("Please enter a word!"),
-                };
-
-                let file = match args.next(){
-                    Some(arg) => arg,
-                    None => return Err("Please enter a file!"),
-                };
-
-                let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-                Ok(Config { 
-                    word,
-                    file,   
-                    ignore_case,
-                })
-            }
-        }
-
-
-        fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
-            let query = query.to_lowercase();
-
-            content
-                .lines()
-                .filter(| line | line.to_lowercase().contains(&query))
-                .collect()
-        }
-
-        
-        
-        fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str>{   
-            content
-                .lines()
-                .filter(|line| line.contains(query))
-                .collect()
-        }
-
-        
-        fn run(config: Config) -> Result<(), Box<dyn Error>>{
-            let content: String = fs::read_to_string(&config.file)?;
-
-            let results = if config.ignore_case{
-                search_case_insensitive(&config.word, &content)
-            }else{
-                search(&config.word, &content)
-            };
-
-            for line in results{
-                    println!("{}", line);
-            } 
-            Ok(())
-        }
+use std::env;
+use std::process;
+use minigrep::Config;
 
         fn main() {
             let config = Config::build(env::args()).unwrap_or_else(|err|{
@@ -75,7 +9,7 @@
             });
                 
             
-            if let Err(e) = run(config){
+            if let Err(e) = minigrep::run(config){
                 eprintln!("Application error: {}", e);
                 process::exit(1);
             }
