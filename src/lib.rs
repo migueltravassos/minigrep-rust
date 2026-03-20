3386ed8727ed81d9ce45d36b555f36dcd25cc27c
Miguel Travassos <miguel.ft1706@gmail.com>
2026-03-20 11:24:49 +0000

    Added enumeration

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,66 @@
-        use std::env;
-        use std::error::Error;
-        use std::fs;
- 
-        pub struct Config{
-            word: String,
-            file: String,
-            ignore_case: bool,
-        }
+use std::env;
+use std::error::Error;
+use std::fs;
 
-        impl Config{
-            pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str>{
-                args.next(); // path
+pub struct Config {
+    word: String,
+    file: String,
+    ignore_case: bool,
+}
 
-                let word = match args.next(){
-                    Some(arg) => arg,
-                    None => return Err("Please enter a word!"),
-                };
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // path
 
-                let file = match args.next(){
-                    Some(arg) => arg,
-                    None => return Err("Please enter a file!"),
-                };
+        let word = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Please enter a word!"),
+        };
 
-                let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let file = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Please enter a file!"),
+        };
 
-                Ok(Config { 
-                    word,
-                    file,   
-                    ignore_case,
-                })
-            }
-        }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
+        Ok(Config {
+            word,
+            file,
+            ignore_case,
+        })
+    }
+}
 
-        fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
-            let query = query.to_lowercase();
+fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<(usize, &'a str)> {
+    let query = query.to_lowercase();
 
-            content
-                .lines()
-                .filter(| line | line.to_lowercase().contains(&query))
-                .collect()
-        }
+    content
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| line.to_lowercase().contains(&query))
+        .collect()
+}
 
-        
-        
-        fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str>{   
-            content
-                .lines()
-                .filter(|line| line.contains(query))
-                .collect()
-        }
+fn search<'a>(query: &str, content: &'a str) -> Vec<(usize,&'a str)> {
+    content
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| line.contains(query))
+        .collect()
+}
 
-        
-        pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-            let content: String = fs::read_to_string(&config.file)?;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content: String = fs::read_to_string(&config.file)?;
 
-            let results = if config.ignore_case{
-                search_case_insensitive(&config.word, &content)
-            }else{
-                search(&config.word, &content)
-            };
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.word, &content)
+    } else {
+        search(&config.word, &content)
+    };
 
-            for line in results{
-                    println!("{}", line);
-            } 
-            Ok(())
-        }
+    for (index,line) in results {
+        println!("Line {}: {} ", index + 1, line);
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 
-        fn main() {
-            let config = Config::build(env::args()).unwrap_or_else(|err|{
-                eprintln!("Error: {}", err);
-                process::exit(1);
-            });
-                
-            
-            if let Err(e) = minigrep::run(config){
-                eprintln!("Application error: {}", e);
-                process::exit(1);
-            }
-        }
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
